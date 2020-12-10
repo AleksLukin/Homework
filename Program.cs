99485dfa308d0c41da552f83d69bcdd1116d7735
AleksLukin <75177874+AleksLukin@users.noreply.github.com>
2020-12-10 23:31:06 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите имя ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Привет, " + name + " сегодня" + "[" + DateTime.Now + "]");
+        }
+    }
+}
